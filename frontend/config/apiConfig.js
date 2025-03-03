04df52e02385aca2ee9ea8cf424b302089bfebef
Gagas Surya Laksana <gagassurya19@gmail.com>
2025-03-03 22:33:03 +0700

    Update apiConfig.js

--- frontend/config/apiConfig.js
+++ frontend/config/apiConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://3471-2a09-bac5-3a10-88c-00-da-e2.ngrok-free.app';
+const baseUrl = 'https://7ba0-2a09-bac5-3a13-88c-00-da-b3.ngrok-free.app';
 const coinMarketCapApiKey = 'd1b90582-1f85-4dee-8c31-2d43c234a453';
 
 export const API_CONFIG = {
