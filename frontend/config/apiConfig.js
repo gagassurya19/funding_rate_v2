5963071e0bae60edb91eac3300459721fb501b39
Gagas Surya Laksana <gagassurya19@gmail.com>
2025-03-03 23:22:50 +0700

    Update apiConfig.js

--- frontend/config/apiConfig.js
+++ frontend/config/apiConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://bab2xff1tfm5.share.zrok.io';
+const baseUrl = 'https://ze799h8j3wap.share.zrok.io';
 const coinMarketCapApiKey = 'd1b90582-1f85-4dee-8c31-2d43c234a453';
 
 export const API_CONFIG = {
